3d9ba91c98b68c8d7c93cdc5f4478d7aabc03484
Vinayak <vinn.g47@gmail.com>
2023-03-13 16:23:18 +0530

    configure PORT

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
