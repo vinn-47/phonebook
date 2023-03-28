ab5b2738be51b0e936fbf582d520e1864eeb93bd
Vinayak <vinn.g47@gmail.com>
2023-03-28 20:24:23 +0530

    added validation for db and eslint

--- index.js
+++ index.js
@@ -30,28 +30,40 @@ app.use(morgan(function (tokens, req, res) {
     return logObj
 }))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformed id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
-]
+
+    next(error)
+}
+
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app.get('/', (req, res) => {
     res.send('<h1>Homepage</h1>')
@@ -65,9 +77,9 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (req, res) => {
     var dateTime = new Date()
-    
+    const len = Person.length
     res.send(`
-        Phonebook has info for ${persons.length} people
+        Phonebook has info for ${len} people
         <br /><br />
         ${dateTime.toDateString()}  ${dateTime.toTimeString()}
     `)
@@ -87,18 +99,21 @@ app.get('/api/persons/:id', (request, response) => {
 //     return getRandomInt(persons.length * 1000)
 // }
 
-app.post('/api/persons', (request, response) => {
-    const body = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
 
-    if(!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
+    Person.findByIdAndUpdate(request.params.id, 
+            { name, numeber }, 
+            { new: true, runValidators: true, context: 'query' }
+        )
+        .then(updatedPerson => {
+            response.json(updatedPerson)
         })
-    } else if(!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
+        .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
 
     // if(persons.find(person => person.name === body.name)) {
     //     return response.status(400).json({
@@ -110,18 +125,23 @@ app.post('/api/persons', (request, response) => {
         name: body.name,
         number: body.number,
     })
-
-    person.save().then(savedPerson => {
+    
+    person.save()
+    .then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- models/person.js
+++ models/person.js
@@ -15,8 +15,20 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2}\d?-\d{5,}$/.test(v)
+     }
+  },
+}
 })
 
 personSchema.set('toJSON', {
